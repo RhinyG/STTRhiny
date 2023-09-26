9c20d54da4bf7b556e900b7c1c2cba916586f65d
Stanislas Tech Team <ftcscw@gmail.com>
2023-09-26 16:30:40 +0200

    Slidesequence nog niet af.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SlideTest.java
@@ -13,13 +13,11 @@ public class SlideTest extends LinearOpMode {
     DrivetrainAlex drivetrain = new DrivetrainAlex();
     Outtake outtake = new Outtake();
 
-
     @Override
     public void runOpMode() throws InterruptedException {
 
         drivetrain.init(hardwareMap);
         outtake.init(hardwareMap);
-        DcMotor hook = hardwareMap.dcMotor.get("slide");
         DcMotor intake = hardwareMap.dcMotor.get("intake");
 
         waitForStart();
@@ -30,29 +28,32 @@ public class SlideTest extends LinearOpMode {
             double x = gamepad1.left_stick_x; // y direction is reversed
             double y = -gamepad1.left_stick_y;
             double rotate = gamepad1.right_stick_x;
-            boolean open = gamepad1.a;
-            boolean closed = gamepad1.b;
+            boolean open = gamepad1.dpad_left;
+            boolean closed = gamepad1.dpad_right;
             boolean up = gamepad1.x;
             boolean down = gamepad1.y;
+            boolean down2 = gamepad1.dpad_down;
+            double slidePosition = 0;
+
 
             double intakePower = gamepad1.right_trigger - gamepad1.left_trigger;
 
             DrivetrainAlex.maxSpeed = 1;
 
             if(gamepad1.left_bumper) {
-                hook.setPower(1);
+                //outtake.slide.setPower(1);
             } else if (gamepad1.right_bumper) {
-                hook.setPower(-1);
+                //outtake.slide.setPower(-1);
             }
             else {
-                hook.setPower(0);
+                //outtake.slide.setPower(0);
             }
 
             intake.setPower(intakePower);
 
             drivetrain.drive(y, x, rotate);
-            outtake.updateLeftClaw(open, closed);
-            outtake.updateLeftRotate(up,down);
+//            outtake.updateLeftClaw(open, closed);
+//            outtake.updateLeftRotate(up,down, down2);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/Outtake.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.robotParts;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -7,28 +8,57 @@ public class Outtake extends RobotPart{
 
     Servo leftClaw;
     Servo leftRotate;
+    DcMotor slide;
     double leftClawPos;
     double leftRotatePos;
 
     public void init(HardwareMap map) {
         leftClaw = map.get(Servo.class, "leftClaw");
         leftRotate = map.get(Servo.class, "leftRotate");
+        slide  = map.dcMotor.get("slide");
     }
 
-    public void updateLeftClaw(boolean up, boolean down) {
-        if (up) {
-            leftClawPos = 1;
-        } else if (down) {
-            leftClawPos = -1;
+    /**
+     *
+     * @param mode 1 = intakePos, 2 = movePos, 3 = outtakePos
+     */
+    public void updateLeftRotate(int mode) {
+        if (mode == 1) {
+            leftClawPos = 1.0;
+        } else if (mode == 2) {
+            leftClawPos = 0.2; //0.5
+        } else if (mode == 3) {
+            leftClawPos = 0.5;
         }
+
         leftClaw.setPosition(leftClawPos);
     }
-    public void updateLeftRotate(boolean up, boolean down) {
-        if (up) {
-            leftRotatePos = 1;
-        } else if (down) {
-            leftRotatePos= -1;
+
+    /**
+     *
+     * @param mode 1 = release, 2 = grab
+     */
+    public void updateLeftClaw(int mode) {
+        if (mode == 1) {
+            leftRotatePos = 0.5;
+        } else if (mode == 2) {
+            leftRotatePos= 0.25;
         }
         leftRotate.setPosition(leftRotatePos);
+    }
+    /**
+     * Hiervoor moet de slides eerst encoder cables hebben.
+     * @param height encoder height
+     */
+    public void outtakeSequence(int height) {
+        updateLeftClaw(1);
+        updateLeftRotate(2);
+//        slide.goToPosition(0);
+        updateLeftRotate(1);
+        updateLeftClaw(2);
+//        slide.goToPosition(height);
+        updateLeftRotate(3);
+        updateLeftClaw(1);
+        updateLeftRotate(2);
     }
 }
