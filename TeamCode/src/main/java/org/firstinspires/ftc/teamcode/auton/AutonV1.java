b7e8735a4ad886ec8d84d6aa958d1ad5d4259f8a
Stanislas Tech Team <ftcscw@gmail.com>
2023-11-16 19:22:37 +0100

    All auton methods work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonV1.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.auton;
+//comment for github
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
