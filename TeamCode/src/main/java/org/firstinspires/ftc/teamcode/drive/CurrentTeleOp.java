3ff94aa97523b27839f70a1144f2dbde48171aef
Stanislas Tech Team <ftcscw@gmail.com>
2023-12-01 15:32:00 +0100

    All auton methods work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CurrentTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CurrentTeleOp.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.drive;
+//comment for Github
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
