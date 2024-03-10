6d2b8804b6019954162c3fd58e5667c19cbe60da
stanislastechacademy <ftc.stanislastechacademy@gmail.com>
2024-03-10 12:03:22 +0100

    for loop init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/DifferentialDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/DifferentialDrivetrain.java
@@ -6,6 +6,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
@@ -13,35 +14,55 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Config
 public class DifferentialDrivetrain {
     public DcMotorEx FrontB,FrontR,BackB,BackR;
+    //TODO: test
+    DcMotorEx[] DTMotors;
+    String[] DTMotorNames = {"blue_front","red_front","blue_back","red_back"};
     private final LinearOpMode myOpMode;
     HardwareMap map;
     Telemetry telemetry;
+    Gamepad gamepad1, gamepad2;
     public IMU imu;
     public static double pb = 0.001, ib, db, pr = 0.001, ir, dr;
     public static int blueHeadingGoal = 0, redHeadingGoal = 0;
     PIDController blueHeading = new PIDController(pb, ib, db), redHeading = new PIDController(pr,ir,dr);
-    int TICKS_PER_ROTATION = 8192;
+    int TICKS_PER_ROTATION = 8192, dPosR,dPosB, redCurrentHeading;
+    double x, y, r, gamepadTheta, pidR, pidB;
     public DifferentialDrivetrain(LinearOpMode opmode) {
         myOpMode = opmode;
+        //TODO: find out why this NPE
         map = opmode.hardwareMap;
         telemetry = opmode.telemetry;
+        //TODO: test for NPE
+        gamepad1 = opmode.gamepad1;
+        gamepad2 = opmode.gamepad2;
     }
 
     /**
-     * This methods initialises the mecanum drivetrain and the IMU and sets all the directions and modes to their correct settings.
+     * This methods initialises the swerve drivetrain and the IMU and sets all the directions and modes to their correct settings.
      */
     public void init() {
         imu = myOpMode.hardwareMap.get(IMU.class, "imu");
 
-        FrontB = myOpMode.hardwareMap.get(DcMotorEx.class, "blue_front");
-        FrontR = myOpMode.hardwareMap.get(DcMotorEx.class, "red_front");
-        BackB = myOpMode.hardwareMap.get(DcMotorEx.class, "blue_back");
-        BackR = myOpMode.hardwareMap.get(DcMotorEx.class, "red_back");
+        DTMotors = new DcMotorEx[]{FrontB, FrontR, BackB, BackR};
+        //TODO: test for NPE
+        for (int i = 0; i < DTMotors.length; i++) {
+            DTMotors[i] = myOpMode.hardwareMap.get(DcMotorEx.class,DTMotorNames[i]);
+            if ((i + 2) % 2 == 0){
+                DTMotors[i].setDirection(DcMotorSimple.Direction.REVERSE);
+            } else {
+                DTMotors[i].setDirection(DcMotorSimple.Direction.FORWARD);
+            }
+        }
 
-        FrontB.setDirection(DcMotorSimple.Direction.REVERSE);
-        FrontR.setDirection(DcMotorSimple.Direction.FORWARD);
-        BackB.setDirection(DcMotorSimple.Direction.REVERSE);
-        BackR.setDirection(DcMotorSimple.Direction.FORWARD);
+//        FrontB = myOpMode.hardwareMap.get(DcMotorEx.class, "blue_front");
+//        FrontR = myOpMode.hardwareMap.get(DcMotorEx.class, "red_front");
+//        BackB = myOpMode.hardwareMap.get(DcMotorEx.class, "blue_back");
+//        BackR = myOpMode.hardwareMap.get(DcMotorEx.class, "red_back");
+//
+//        FrontB.setDirection(DcMotorSimple.Direction.REVERSE);
+//        FrontR.setDirection(DcMotorSimple.Direction.FORWARD);
+//        BackB.setDirection(DcMotorSimple.Direction.REVERSE);
+//        BackR.setDirection(DcMotorSimple.Direction.FORWARD);
 
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
         RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
@@ -53,17 +74,17 @@ public class DifferentialDrivetrain {
     }
 
     public void swerveSimple(){
-        FrontB.setPower(1*myOpMode.gamepad1.left_stick_y);
-        FrontR.setPower(1*myOpMode.gamepad1.left_stick_x);
-        BackB.setPower(1*myOpMode.gamepad1.right_stick_y);
-        BackR.setPower(1*myOpMode.gamepad1.right_stick_x);
+        FrontB.setPower(1*gamepad2.left_stick_y);
+        FrontR.setPower(-1*gamepad2.left_stick_x);
+        BackB.setPower(1*gamepad2.right_stick_y);
+        BackR.setPower(-1*gamepad2.right_stick_x);
     }
 
     public void swerveRobotCentric(){
-        double x = myOpMode.gamepad1.left_stick_x;
-        double y = -myOpMode.gamepad1.left_stick_y;
-        double r = Math.sqrt(x * x + y * y);
-        double gamepadTheta;
+        x = gamepad1.left_stick_x;
+        y = -gamepad1.left_stick_y;
+        r = Math.sqrt(x * x + y * y);
+        redCurrentHeading = FrontR.getCurrentPosition();
 
         if (x >= 0 && y >= 0) {
             gamepadTheta = Math.atan(y / x);
@@ -77,9 +98,16 @@ public class DifferentialDrivetrain {
             redHeadingGoal = (int) ((gamepadTheta-0.5*Math.PI)/(2*Math.PI)*TICKS_PER_ROTATION);
         }
 
+        dPosR = redHeadingGoal - redCurrentHeading;
+
+        if (dPosR < -0.5 * TICKS_PER_ROTATION) {
+            redHeadingGoal -= TICKS_PER_ROTATION;
+        } else if (dPosR > 0.5 * TICKS_PER_ROTATION) {
+            redHeadingGoal += TICKS_PER_ROTATION;
+        }
+
         redHeading.setPID(pr,ir,dr);
-        int redCurrentHeading = FrontR.getCurrentPosition();
-        double pidR = redHeading.calculate(redCurrentHeading,redHeadingGoal);
+        pidR = redHeading.calculate(redCurrentHeading,redHeadingGoal);
 
         FrontR.setPower(pidR);
         BackR.setPower(pidR);
